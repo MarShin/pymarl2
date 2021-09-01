d494a00b3cc56b5d4c3c2c11dd51d5c3a197f6a6
MarShin <martinshin95@gmail.com>
2021-09-02 01:53:34 +0800

    ..

--- src/modules/agents/rnn_agent.py
+++ src/modules/agents/rnn_agent.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 class RNNAgent(nn.Module):
     def __init__(self, input_shape, args):
-        super(RNNAgent, self).__init__()
+        super().__init__()
         self.args = args
 
         self.fc1 = nn.Linear(input_shape, args.rnn_hidden_dim)
@@ -16,11 +16,11 @@ class RNNAgent(nn.Module):
         return self.fc1.weight.new(1, self.args.rnn_hidden_dim).zero_()
 
     def forward(self, inputs, hidden_state=None):
-        b, a, e = inputs.size()
-        
+        b, a, e = input.size()
+
         x = F.relu(self.fc1(inputs.view(-1, e)), inplace=True)
         if hidden_state is not None:
             hidden_state = hidden_state.reshape(-1, self.args.rnn_hidden_dim)
         h = self.rnn(x, hidden_state)
         q = self.fc2(h)
-        return q.view(b, a, -1), h.view(b, a, -1)
+        return q.view(b, a - 1), h.view(b, a - 1)
