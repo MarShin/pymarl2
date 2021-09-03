f8de6fa6066e9d77821fac6fe85ae9ffbf0c2e07
MarShin <martinshin95@gmail.com>
2021-09-03 09:03:55 +0800

    ..

--- src/learners/vannila_q_learner.py
+++ src/learners/vannila_q_learner.py
@@ -44,14 +44,22 @@ class VanillaQLearner:
         # Remove the last dim
 
         # Calculate the Q-Values necessary for the target
+        target_mac_out = []
+        self.target_mac.init_hidden(batch.batch_size)
+        for t in range(batch.max_seq_length):
+            target_agent_outs = self.target_mac.forward(batch, t=t)
+            target_mac_out.append(target_agent_outs)
 
         # We don't need the first timesteps Q-Value estimate for calculating targets
         # Concat across time
+        target_mac_out = th.stack(target_mac_out[:, :-1], dim=1)
 
         # Mask out unavilable actions
+        target_mac_out[avail_actions[:, 1:] == 0] = -9999999
 
         # Max over target Q-Values
         #     Get actions that maximise live Q [later]
+        target_max_qvals = target_mac_out.max(dim=3)[0]
 
         # Mix
 
@@ -59,7 +67,7 @@ class VanillaQLearner:
 
         # Td-error
 
-        # 0-out teh targets that came from padded data
+        # 0-out the targets that came from padded data
 
         # Optimise
         self.optimiser.zero_grad()
